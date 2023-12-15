520f54d59ebb4e65684e511ecad65aa8aa923401
Loghin Vlad-Ștefan <stefan10vlad@gmail.com>
2023-12-15 13:30:50 +0200

    initializează subprograme ex1

--- ex1.py
+++ ex1.py
@@ -1,2 +1,22 @@
 #1) Verificați dacă inversa unei matrici triunghiulare este tot
 #triunghiulară. 
+
+
+def ver_triung_sup(mat): #verifică dacă matricea este triunghiulară superior
+    return
+
+def ver_triung_inf(mat): #verifică dacă matricea este triunghiulară inferior
+    return
+
+def calc_det(mat): #calculează determinantul matricii
+    return
+
+def inv_matrice(mat): #inversează matricea
+    return
+
+if __name__ == "__main__":
+    n=input("Introduceți mărimea matricii: ")
+
+    if (n<=1) or not(isinstance(n, int)):
+        print("Mărimea matricii nu e validă. Trebuie să fie un număr natural, mai mare de 1.")
+        sys.exit(0)
